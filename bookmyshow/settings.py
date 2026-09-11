51f22e0bbcf09ae85d7080277a205dd26b05ec45
AayeshaSaiyed <aayeshasaiyed76@gmail.com>
2026-09-11 23:09:46 +0530

    Update database settings

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'bookmyshow.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default='sqlite:///db.sqlite3'
     )
     }
 
